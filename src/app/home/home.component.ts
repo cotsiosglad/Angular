90d30ca37133546757b87f9982fed9d2d759f14a
cotsiosglad <c.constantinides95@gmail.com>
2023-07-02 20:10:33 +0300

    Added changes up to Routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -18,20 +19,20 @@ import { Housinglocation } from '../housinglocation';
     </form>
   </section>
   <section class="results">
-    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+  <app-housing-location
+  *ngFor="let housingLocation of housingLocationList"
+  [housingLocation]="housingLocation">
+</app-housing-location>
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  housingLocation: Housinglocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: 'assets/house1.jpg',
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+  
 }
