f636e365d1390319f94f2b8d11a54716226c3021
Nikolas09 <nikolas_erotokritou@hotmail.co.uk>
2023-07-02 17:26:16 +0300

    html template added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeModule } from './pages/Home/home.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeModule],
+  
   template: `<h1>Default</h1>`,
-  styleUrls: ['./app.component.css'],
+  // templateUrl: '../index.html',
+  // templateUrl: 'pages\\Home\\home.component.html',
+  styleUrls: ['./app.component.css','./pages/Home/home.component.css',
+
+],
 })
 export class AppComponent {
   title = 'default';
